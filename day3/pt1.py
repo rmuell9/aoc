696c47a9440fd1c726c84cf9c41a855b9f242706
rmuell9 <rmatthewmueller@gmail.com>
2025-12-03 04:06:33 -0600

    fixes

--- day3/pt1.py
+++ day3/pt1.py
@@ -1,7 +1,7 @@
 with open("assets/submit.txt", "r", encoding="utf-8") as f:
     raw_text = f.read().split("\n")[:-1]
 
-res = list()
+res = int()
 for bank in raw_text:
     jolt = str()
     if bank.index(max(bank)) != len(bank)-1:
@@ -10,6 +10,6 @@ for bank in raw_text:
         jolt += max(bank)
     else:
         jolt += max(bank[:-1]) + max(bank)
-    res.append(int(jolt))
+    res += int(jolt)
 
-print(sum(res))
+print(res)
