ce113f7f1957baafe3ae59763741a1e1b070674b
rmuell9 <rmatthewmueller@gmail.com>
2025-12-06 01:29:46 -0600

    day6

--- day6/p2.py
+++ day6/p2.py
@@ -1,2 +1,44 @@
-with open("assets/test.txt", "r", encoding="utf-8") as f:
+with open("assets/submit.txt", "r", encoding="utf-8") as f:
     raw_text = f.read().splitlines()
+
+res = int()
+ref = str()
+
+for i in range(len(raw_text[0])-1, -1, -1):
+    for row in raw_text:
+        try:
+            ref += row[i]
+        except:
+            ref += " "
+
+ref = ref.split(" ")
+op = list()
+
+for a in ref:
+
+    if a == "":
+        continue
+
+    if "*" in a:
+        mult = 1
+        a = a.strip("*")
+        if a != "":
+            op.append(int(a))
+        for num in op:
+            mult *= num
+
+        res += mult
+        op = list()
+
+    elif "+" in a:
+        a = a.strip("+")
+        if a != "":
+            op.append(int(a))
+
+        res += sum(op)
+        op = list()
+
+    else:
+        op.append(int(a))
+
+print(res)
